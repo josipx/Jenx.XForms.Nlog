772aa78c5a2e2741c4146b22f64f45c4c5310618
Joze Jenkole <joze.jenkole@gmail.com>
2020-07-11 10:38:11 +0200

    initial commit

---
+++ Jenx.XForms.Nlog/NLogPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using NLog;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Jenx.XForms.Nlog
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NLogPage : ContentPage
+    {
+        public NLogPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var logger = LogManager.GetCurrentClassLogger();
+            logger.Info("Jenx.si was here");
+        }
+    }
+}
